813d0998b02efac3b3b2b3b39fa4876c66ce3976
rluisr <detteiu.x14th@gmail.com>
2016-03-01 14:45:16 +0900

    文字化けするンゴ

--- getTorList.php
+++ getTorList.php
@@ -1,4 +1,23 @@
 <?php
 
-$tor = file_get_contents("https://www.dan.me.uk/torlist/");
-file_put_contents("tornodelist", $tor);
+$url = "https://www.dan.me.uk/tornodes";
+
+$headers = array(
+    "HTTP/1.1",
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding:gzip ,deflate",
+    "Accept-Language:ja,en-us;q=0.7,en;q=0.3",
+    "Connection:keep-alive",
+    "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:26.0) Gecko/20100101 Firefox/26.0"
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$html =  curl_exec($ch);
+curl_close($ch);
+
+echo $html;
+
+file_put_contents("tornodelist", $html);
