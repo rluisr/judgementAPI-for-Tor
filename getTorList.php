1333296aac09d3c3cbe22974289cb294f8083a3e
rluisr <detteiu.x14th@gmail.com>
2016-03-01 15:49:35 +0900

    エラー処理

--- getTorList.php
+++ getTorList.php
@@ -17,9 +17,12 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_ENCODING, "gzip");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-$html =  curl_exec($ch);
+
+$html = curl_exec($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
 curl_close($ch);
 
-echo $html;
+if ($code === "403") { die(); }
 
 file_put_contents("tornodelist", $html);
